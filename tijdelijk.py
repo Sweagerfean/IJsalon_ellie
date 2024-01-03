982deb9b24c88c421fb0809e8da8d05300695faa
Janny van der Heide <ja.vanderheide@achtkarspelen.nl>
2024-01-03 17:40:41 +0100

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,25 @@
+prijzen={
+    "aardbei" : 3,
+    "vanille" : 4,
+    "chocolade" : 5
+}
+
+aanbieding = prijzen["aardbei"] * 0.8
+
+
+reclame_tekst = (f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}")
+
+reclame_tekst2 = reclame_tekst[:63]
+
+reclame_tekst3 = reclame_tekst2.upper ()
+
+reclame_tekst4 = reclame_tekst3.split(" ")
+
+el = reclame_tekst4
+for i in el:
+ if len(i) >= 5:
+  print (i.upper())
+ else:
+  print (i.lower())
+
+
